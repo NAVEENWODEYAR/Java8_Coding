9264840726523bd43afba24fc5d575baf2cce265
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-25 17:42:23 +0530

    map() used for Collectors.partioningBY()
    separate odd and even numbers

--- src/main/java/com/coding/arrays/seperateOddEven.java
+++ src/main/java/com/coding/arrays/seperateOddEven.java
@@ -5,14 +5,14 @@ package com.coding.arrays;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 /**
  * @author Naveen K Wodeyar
  * @apiNote Given a list of integers, separate odd and even numbers
  */
 public class seperateOddEven {
 
-    static void oddEven() {
-        List<Integer> intList = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
+    static void oddEven(List<Integer> intList) {
         		
         Map<Boolean, List<Integer>> collect = intList.stream().collect(Collectors.partitioningBy(n-> n%2==0));
         
@@ -28,8 +28,17 @@ public class seperateOddEven {
         }
     
     }
+    
+    static void oddEvenSeperator(List<Integer> intList) {
+    	Stream<Object> collect = intList.parallelStream().collect(Collectors.partitioningBy(n->n%2==0))
+    		.entrySet().stream()
+    		.map(k->String.format("%n%s",k.getKey()?"Even":"Odd",k.getValue().stream().map(String::valueOf).collect(Collectors.joining("\n"))));
+    	System.out.println(collect);
+    }
     public static void main(String[] args) {
-	oddEven();
+        List<Integer> intList = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
+//        oddEven(intList);
+        oddEvenSeperator(intList);
     }
 
 }
