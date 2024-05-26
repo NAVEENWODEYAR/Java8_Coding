da3ea49dc9fca12a55e63d53e57870f5ecd179b2
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-26 16:38:44 +0530

    Anagrams in java8

--- src/main/java/com/coding/arrays/MegeArrays.java
+++ src/main/java/com/coding/arrays/MegeArrays.java
@@ -1,6 +1,9 @@
 
 package com.coding.arrays;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * @author Naveen K Wodeyar,
  * @date 25-05-2024,
@@ -8,10 +11,15 @@ package com.coding.arrays;
 public class MegeArrays {
 	
 	static void mergeArrays() {
+		int[] a = new int[] {4, 2, 7, 1,2,4,2,9};
+        int[] b = new int[] {8, 3, 9, 5,9,2,4,5};
+        
+        int[] array = IntStream.concat(Arrays.stream(a), Arrays.stream(b)).sorted().distinct().toArray();
+        System.out.println(Arrays.toString(array));
 		
 	}
 	
 	public static void main(String[] args) {
-		
+		mergeArrays();
 	}
 }
---
+++ src/main/java/com/coding/string/Anagrams.java
@@ -0,0 +1,27 @@
+
+package com.coding.string;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @date 26/05/2024 16:26PM
+ * @apiNote Java 8 program to check if two strings are anagrams or not
+ * Two strings are anagrams if they contain the same characters with the same frequency,
+ */
+public class Anagrams {
+
+	static void anagramTest() {	
+		 String s1 = "RaceCar",s2="CarRace";
+
+		 boolean equals = Stream.of(s1.toLowerCase().split(""))
+		 		.sorted().collect(Collectors.joining())
+		 		.equals(Stream.of(s2.toLowerCase().split("")).sorted()
+		 				.collect(Collectors.joining()));
+		 System.out.println(equals?"True":"False");
+	}
+	public static void main(String[] args) {
+		anagramTest();
+	}
+
+}
