20191bb5d8550cd0c146762eaf6a143b5bcd9b7c
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-29 09:10:19 +0530

    series using java8

--- src/main/java/com/coding/series/FibonacciSeries.java
+++ src/main/java/com/coding/series/FibonacciSeries.java
@@ -3,6 +3,7 @@ package com.coding.series;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.stream.Stream;
 
@@ -14,16 +15,17 @@ import java.util.stream.Stream;
  */
 public class FibonacciSeries {
 	static void fiboSeries() {
-		Stream.iterate(new int[] {0,1}, n-> new int[] {n[1],n[0]+n[1]}).limit(10).map(n->n[0]).forEach(System.out::println);
+		Stream.iterate(new int[] {0,1}, n-> new int[] {n[1],n[0]+n[1]}).limit(10).map(n->n[0]).forEach(i-> System.out.print(i+","));
 	}
 	
-	static void dateCalci() {
-		LocalDate date = LocalDate.of(1996,05,06);
+	static void dateCalci(LocalDate date) {
 		LocalDate now = LocalDate.now();
 		System.out.println(ChronoUnit.YEARS.between(date, now));
+
 	}
 	
 	public static void main(String[] args) {
 		fiboSeries();
+		dateCalci(LocalDate.of(1996,05,06));
 	}
 }
