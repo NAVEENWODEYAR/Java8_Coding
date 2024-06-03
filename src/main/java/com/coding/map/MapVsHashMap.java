54093f9aad8aa8c7c90833772b451dbcffe735f6
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-03 23:37:07 +0530

    map() vs flatmap()

--- src/main/java/com/coding/map/MapVsHashMap.java
+++ src/main/java/com/coding/map/MapVsHashMap.java
@@ -1,8 +1,8 @@
 
 package com.coding.map;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.*;
 
 public class MapVsHashMap {
 	/**
@@ -10,15 +10,30 @@ public class MapVsHashMap {
 	 * @date 03-Jun-202411:08:43 pm
 	 * @classDef,  Differences Between Java 8 Map() And flatMap()
 	 */
-
-	static List<Institute> instList = new ArrayList<>(10)
-	static void mapDemo() {
-		
+	 static List<Institute> instList = new ArrayList<>(10);
+	static {
+		instList.add(new Institute("IIT",Arrays.asList("Bombay","Chennai","Karagpur","IIT Delhi")));
+		instList.add(new Institute("IIM", Arrays.asList("Bangalore", "Ahmedabad", "Kozhikode", "Lucknow")));
 	}
 	
-	static void falatMp() {}
-	public static void main(String[] args) {
+	//names of all institutes,
+	static void mapDemo() {
+		List<String> list = instList.stream().map(Institute::getInstName).toList();
+		System.out.println("Institute names :"+list);
+		falatMp();
 
+	}
+	
+	// Get unique locations of all institutes,
+	static void falatMp() {
+    instList.stream().flatMap(institute -> institute.getLocations().stream()).collect(Collectors.toSet());
+
+		System.out.println("Instituet address :");
+	}
+	
+	
+	public static void main(String[] args) {
+		mapDemo();
 	}
 
 }
