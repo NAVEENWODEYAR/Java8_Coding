e38f3a3de502f97354aafb224b3c2300ad3b511f
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-05 11:42:08 +0530

    mapping using collectors.toMap()

--- src/main/java/com/coding/streams/StremOperations.java
+++ src/main/java/com/coding/streams/StremOperations.java
@@ -2,6 +2,7 @@
 package com.coding.streams;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Naveen K Wodeyar
@@ -10,14 +11,27 @@ import java.util.*;
  */
 public class StremOperations {
 	
-	static List<String> list = Arrays.asList("One","Two","Three","Four");
+	static List<String> list = Arrays.asList("One","Two","Three","Four","Five","One","Two","Three");
 	// filter(),
 	static List filterStream() {
 		return list.stream().filter(n->n.length() >=4).toList();
 	}
 	
+	// distinct() : selects only unique elements
+	static List distint() {
+		return list.stream().distinct().collect(Collectors.toList());
+	}
+	
+	// mapping operations
+	static void mapFn() {
+		List<String> strList = Arrays.asList("One","Two,Three","Four","Five");
+		strList.stream().collect(Collectors.toMap(st->st,st->st.length())).entrySet().forEach(System.out::println);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(filterStream());
+		System.out.print(distint());
+		mapFn();
 	}
 
 }
