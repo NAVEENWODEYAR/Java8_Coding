f422d5d8ea582ad65d85ace37fff86a0c3813e9f
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-09 21:03:37 +0530

    Exception in thread main java.lang.ClassCastException: class java.util.HashMap cannot be cast to class java.lang.Comparable (java.util.HashMap and java.lang.Comparable are in module java.base of loader 'bootstrap')

--- src/main/java/com/coding/map/Characterfrequency.java
+++ src/main/java/com/coding/map/Characterfrequency.java
@@ -1,7 +1,9 @@
 
 package com.coding.map;
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author Naveen K Wodeyar
@@ -17,11 +19,27 @@ public class Characterfrequency {
 			hashMap.put(c, hashMap.getOrDefault(c, 0)+1);
 		}
 		System.out.println(hashMap);
-		
+		charFreq(st);
 	}
+	
+	static void charFreq(String st) {
+		st.chars().filter(c->c!=' ').mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting())).entrySet().forEach(System.out::println);
+	}
+	
+	static void wordFreq(List<String> list) {
+		list.stream()
+			.collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+			.entrySet().stream().sorted()
+			.forEach(System.out::println);
+	}
+	
 	public static void main(String[] args) {
 		String st = "Programming";
+		List<String> list = Arrays.asList("One","Two","Three","Four","Five","Two","Six","Seven","Four");
+		System.out.println("\n********\n");
 		charFrequency(st);
+		System.out.println("\n********\n");
+		wordFreq(list);
 	}
 
 }
