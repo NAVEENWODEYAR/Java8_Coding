b015a3608327962d275a12316e8529e03c6189af
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-13 23:58:51 +0530

    logOut

--- src/main/java/com/coding/series/PrimeNumber.java
+++ src/main/java/com/coding/series/PrimeNumber.java
@@ -13,8 +13,25 @@ public class PrimeNumber {
 	static boolean primeNo(int n) {
 		return n>1 && IntStream.range(2, (int)Math.sqrt(n)).noneMatch(a->n%a==0);
 	}
+	
+	static boolean prmNo(int n) {
+		
+		if(n==0 || n ==1) {
+			return false;
+		}
+		if(n==2) {
+			return true;
+		}
+		for(int i=2; i<= n/2; i++) {
+			if(n%i==0)
+				return false; 
+		}
+		return true;
+		
+	}
 	public static void main(String[] args) {
 		System.out.println(primeNo(3));
+		System.out.println(prmNo(3));
 	}
 
 }
