388b144516652096042af5847af3b7ee380081b8
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-17 00:03:28 +0530

    binarySearch using java8

--- src/main/java/com/coding/arrays/BinarySearch.java
+++ src/main/java/com/coding/arrays/BinarySearch.java
@@ -1,31 +1,42 @@
 package com.coding.arrays;
 
+import java.util.stream.IntStream;
+
 /**
  * @author Naveen K Wodeyar LENOVO
  * @date 16-Jun-202411:51:08 pm
  * @classDef com.coding
  */
-public class BinarySearch {
-	static int binarySearch(int[] arry ,int low, int high, int key) {
-		int mid = (low+high)/2;
-		
-		if(low <= high) {
-			if(arry[mid] < key) {
-				low = mid+1;
-			}else if(arry[mid] == key) {
-				return mid;
-			}else {
-				high = mid-1;
-			}
-			mid = (low+high)/2;
-		}
-		if(low >high) {
-			return -1;
-		}
-		return -1;
-	}
-	
-	public static void main(String[] args) {
+	public class BinarySearch {
+	    public static int binarySearch(int[] arr, int low, int high, int key) {
+	        int mid;
+	        while (low <= high) {
+	            mid = (low + high) / 2;
+	            if (arr[mid] < key) {
+	                low = mid + 1;
+	            } else if (arr[mid] == key) {
+	                return mid;
+	            } else {
+	                high = mid - 1;
+	            }
+	        }
+	        return -1; 
+	    }
 
+	    static int binarySrch(int[] arr,int key) {
+	    	return IntStream.rangeClosed(0, arr.length-1).filter(i->arr[i]==key).findFirst().getAsInt();
+	    }
+	    
+	    public static void main(String[] args) {
+	        int[] arr = {1, 3, 5, 7, 9, 11, 13, 15};
+	        int key = 7;
+	        int result = binarySearch(arr, 0, arr.length - 1, key);
+	        if (result != -1) {
+	            System.out.println("Element found at index: " + result);
+	        } else {
+	            System.out.println("Element not found");
+	        }
+	        System.out.println("\n************\n");
+	        System.out.println(binarySrch(arr, key));
+	    }
 	}
-}
