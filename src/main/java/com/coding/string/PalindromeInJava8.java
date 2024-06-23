d6973d8e5c5ab6f60ee4316e79406e3f40fff64b
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-23 11:49:23 +0530

    palindrome using loop

--- src/main/java/com/coding/string/PalindromeInJava8.java
+++ src/main/java/com/coding/string/PalindromeInJava8.java
@@ -14,8 +14,27 @@ public class PalindromeInJava8 {
 	static Boolean palindromeTest() {
 		return IntStream.range(0, str.length()/2).allMatch(s->str.toLowerCase().charAt(s) == str.toLowerCase().charAt(str.length()-s-1));
 	}
+	
+	static boolean palindrome(String st) {
+		StringBuilder sb = new StringBuilder(st.toLowerCase());
+		return st.toLowerCase().equals(sb.reverse().toString());
+	}
+	
+	static boolean palindromeLoop() {
+		String rev = "";
+		for(int i=str.length()-1; i>=0; i--) {
+			rev +=str.charAt(i);
+		}
+		String st = rev.toLowerCase();
+		if(st.equalsIgnoreCase(str))
+			return true;
+		return false;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(palindromeTest()?"Palindrome":"Not palindrome");
+		System.out.println(palindromeTest()?"Palindrome":"Not palindrome\n");
+		System.out.println(palindrome(str)?"Palindrome":"Not palindrome");
+		System.out.println(palindromeLoop()?"Palindrome":"Not palindrome");
 	}
 
 }
