7fe56728b77c9ba9155bfd3282f1db87d659761d
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-30 21:32:40 +0530

    4minar

--- src/main/java/com/coding/streams/TreamOperations.java
+++ src/main/java/com/coding/streams/TreamOperations.java
@@ -22,10 +22,12 @@ public class TreamOperations {
         Collections.sort(intList, Comparator.comparingInt(String::length).thenComparing(Comparator.naturalOrder()));
         System.out.println(intList);
         
-        // filter songs,
+        // filtering,
         List<String> filteredList = intList.stream().filter(s->s.length()> 5).sorted().toList();
         System.out.println("\n Filtered list "+filteredList);
         
+        List<String> nthElement = intList.stream().sorted(Comparator.comparing(String::length)).skip(2).limit(1).toList();
+        System.out.println("\nNth Element "+nthElement);
 	}
 	
 	public static void main(String[] args) {
